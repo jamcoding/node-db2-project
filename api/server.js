f29ea522662a2a81c83b34f6fa2c371777af5ba7
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-05-22 21:30:27 -0500

    added .db3 file with migration

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-server.use('/api/cars/', carRouter);
+server.use('/api/cars', carRouter);
 
 server.get('/', (req, res) => {
     res.send('<h2>API is up</h2>');
