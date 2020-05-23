e244f7da344feed9b01b3c5e61d1c60e32ec9dde
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-05-22 22:32:30 -0500

    finished CRUD

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -24,4 +24,49 @@ router.post('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('cars').where({ id })
+        .then(car => {
+            res.status(200).json(car)
+        })
+        .catch(error => {
+            res.status(500).json({ message: "this car with this ID doesn't exist" })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    db('cars').where({ id }).update(changes)
+        .then(carUpdate => {
+            if (carUpdate) {
+                res.status(200).json(carUpdate)
+            } else {
+                res.status(404).json({ message: "this car couldn't be found "})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error updating the car info' })
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('cars').where({ id }).del()
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: 'this car has been deleted' })
+            } else {
+                res.status(404).json({ message: 'this car cannot be found '})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "this car with this ID doesn't exist" })
+        }) 
+})
+
 module.exports = router;
